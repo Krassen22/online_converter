73710b62623a6a09341739e470c794f463879883
Kalin97 <kalo150@abv.bg>
2015-04-02 18:42:11 +0300

    adding style

--- convertor/app/helpers/application_helper.rb
+++ convertor/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def nav_link link_text, link_path, method: :get
+		link_to link_text, link_path, method: method
+	end
+
+	def get_user
+		user, level = user_signed_in? ? [current_user.email, current_user.level] : ["Guest", "none"]
+		"Welcome <span>#{ user }</span> | Level: <span>#{ level }</span>".html_safe
+	end
 end
