5c96e0dd4ad78d9c970b21890e0bbdcebe0ae9e2
Kalin97 <kalo150@abv.bg>
2015-04-03 12:25:32 +0300

    Current page dynamic style

--- convertor/app/helpers/application_helper.rb
+++ convertor/app/helpers/application_helper.rb
@@ -1,10 +1,18 @@
 module ApplicationHelper
 	def nav_link link_text, link_path, method: :get
-		"<li>#{ link_to link_text, link_path, method: method }</li>".html_safe
+		link = link_to link_text, link_path, method: method
+		(current_page?(link_path) ? "<li class='current_page_item'>#{ link }</li>" : "<li>#{ link }</li>").html_safe
 	end
 
 	def get_user
 		user, level = user_signed_in? ? [current_user.email, current_user.level] : ["Guest", "none"]
 		"Welcome <span>#{ user }</span> | Level: <span>#{ level }</span>".html_safe
+	end
+
+	def set_messages
+		messages = ""
+		messages.concat("<div class='alert-success'>#{ notice }</div>") if notice
+		messages.concat("<div class='alert-danger'>#{ alert }</div>") if alert
+		messages.html_safe
 	end
 end
