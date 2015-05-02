403962f52417101340d669171d4f1c7b015c564f
Kalin97 <kalo150@abv.bg>
2015-05-02 14:39:05 +0300

    Added form style

--- convertor/app/helpers/users_helper.rb
+++ convertor/app/helpers/users_helper.rb
@@ -1,2 +1,16 @@
 module UsersHelper
+
+  def devise_error_messages_c!
+    return "" if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    html = <<-HTML
+    <div id="error_explanation">
+      <ul>#{messages}</ul>
+    </div>
+    HTML
+
+    html.html_safe
+  end
+
 end
