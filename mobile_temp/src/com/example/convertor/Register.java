876adc62e3b243e1f6300e57ccc36cd5f3bc65f6
BRANIAC <dimmat97@gmail.com>
2015-05-08 02:13:20 +0300

    Added temp mobile project

---
+++ mobile_temp/src/com/example/convertor/Register.java
@@ -0,0 +1,53 @@
+package com.example.convertor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * Created by dimmat97 on 5/8/15.
+ */
+public class Register extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_screen);
+
+    }
+
+//    @Override
+//    public void finish() {
+//        super.finish();
+//    }
+}
+
+//public class MainActivity extends ActionBarActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+//}
